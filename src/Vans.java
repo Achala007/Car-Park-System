c2c69c9dc616a745fe2e36a37e2c3f7936a32259
Achala007 <achalayasas007@gmail.com>
2017-01-13 09:42:40 +0530

    cargo volume set 2

--- src/Vans.java
+++ src/Vans.java
@@ -3,7 +3,13 @@ public class Vans extends Vehicle {
 
 	private String cargoVolume;
 
-	
+	public String getCargoVolume() {
+		return cargoVolume;
+	}
+
+	public void setCargoVolume(String vol) {
+		this.cargoVolume = vol;
+	}
 
 	@Override
 	public String getIpPlate() {

d77496cb4b035cea7c97547ba38f28d8d67e2c9b
Achala007 <achalayasas007@gmail.com>
2017-01-25 10:05:32 +0530

    interface impl

--- src/carParkManager.java
+++ src/carParkManager.java
@@ -1,15 +1,2 @@
-
-public interface carParkManager {
-	
-	public void addVehicle();
-
-	public void deleteVehicle();
-
-	public void List();
-
-	public void Stat();
-
-	public void MaxANDMin();
-
-	public void History();
+;
 }
